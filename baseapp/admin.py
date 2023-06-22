0d5933cca3b2f83d67d0f6dfb31226f2ffe9345e
Sudarshan519 <sudarshanshrestha519@gmail.com>
2023-06-23 00:01:57 +0545

    admin for currency admin

--- baseapp/admin.py
+++ baseapp/admin.py
@@ -7,3 +7,18 @@ class ContactAdmin(admin.ModelAdmin):
     list_display = ['email','message']
     
 admin.site.register(Contact,ContactAdmin)
+
+# @admin.register(PostalCode)
+# class PostalCodeAdmin(admin.ModelAdmin):
+#     list_display=[k.name  for k in PostalCode._meta.fields]
+# admin.site.register(CurrencyRate)
+# @admin.register(CurrencyRate)
+class CurrencyRateAdmin(admin.ModelAdmin):
+    # list_display=["name","created_at","updated_at"]
+    list_display=[k.name  for k in CurrencyRate._meta.fields]
+    # list_display=[k for k in CurrencyRate._meta.get_fields()]
+    # list_dispaly=('sell')#(k for k in CurrencyRate._meta.get_fields())
+    list_filter = ('name','buy','sell', 'created_at','updated_at',)
+    # list_display=['name','buy','sell', 'created_at','updated_at',]
+    search_fields = ("name",'iso3')
+admin.site.register(CurrencyRate,CurrencyRateAdmin)
