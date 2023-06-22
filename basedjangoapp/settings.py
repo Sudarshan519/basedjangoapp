11be57b383b4b00732ddec8d5736973b93ee35d4
Sudarshan519 <sudarshanshrestha519@gmail.com>
2023-06-22 14:32:23 +0545

    statif files changed

--- basedjangoapp/settings.py
+++ basedjangoapp/settings.py
@@ -133,8 +133,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
-PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Default primary key field type
