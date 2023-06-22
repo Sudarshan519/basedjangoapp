5b437458211b2e7abe2eaa41133a6c8159bae057
Sudarshan519 <sudarshanshrestha519@gmail.com>
2023-06-22 12:01:17 +0545

    contact model added

--- baseapp/models.py
+++ baseapp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Contacts(models.Model):
+    email = models.CharField(max_length=256,)
+    message = models.CharField(max_length=256)
