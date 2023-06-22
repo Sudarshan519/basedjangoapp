a4123e913399c2cbf6a8ff7e79c5d5bc37b80f53
Sudarshan519 <sudarshanshrestha519@gmail.com>
2023-06-22 15:12:37 +0545

    static files udpated

--- basedjangoapp/urls.py
+++ basedjangoapp/urls.py
@@ -17,8 +17,9 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
+
 from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('baseapp.urls'))
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
