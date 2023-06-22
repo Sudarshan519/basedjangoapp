be11646744888e0c8ea5d94ee63b498c383c1397
Sudarshan519 <sudarshanshrestha519@gmail.com>
2023-06-22 15:34:31 +0545

    static files in debug

--- basedjangoapp/urls.py
+++ basedjangoapp/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
+from django.views.static import serve 
 
 from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('baseapp.urls'))
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('',include('baseapp.urls')),
+    path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)#+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
