c3f74353fd782dd46d880f5c7ca24f28ef894e36
Sudarshan519 <sudarshanshrestha519@gmail.com>
2023-06-22 14:02:14 +0545

    messages added on index page

--- baseapp/views.py
+++ baseapp/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.decorators.csrf import csrf_exempt
 from . models import Contacts
 
 
 # Create your views here.
 def index(request):
-    return render(request,'baseapp/index.html')
+    contactlist=Contacts.objects.all()
+
+    return render(request,'baseapp/index.html',{"contacts":contactlist})
 
 @csrf_exempt
 def contactSubmit(request):
@@ -13,4 +15,9 @@ def contactSubmit(request):
     contact.email=request.POST['email']
     contact.message=(request.POST['message'])
     contact.save()
-    return render(request,'baseapp/index.html',{"message":"Your message have been saved successfully"})
+    contactlist=Contacts.objects.all()
+
+    # 
+    # return redirect("index", )
+# {"message":"Your message have been saved successfully"}
+    return render(request,'baseapp/index.html',{"contacts":contactlist,"message":"Your message have been saved successfully"})
