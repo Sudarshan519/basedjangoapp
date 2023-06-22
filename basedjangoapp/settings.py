856c7ef82221456d1d6d0388cad370c05147c5b4
Sudarshan519 <sudarshanshrestha519@gmail.com>
2023-06-22 14:36:25 +0545

    udpate

--- basedjangoapp/settings.py
+++ basedjangoapp/settings.py
@@ -141,3 +141,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"), # your static/ files folder
+]
