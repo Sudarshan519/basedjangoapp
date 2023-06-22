355eaf56c6fe35d7030b5b9a9c6174792d58b0cf
Sudarshan519 <sudarshanshrestha519@gmail.com>
2023-06-22 14:24:37 +0545

    changes updated

--- basedjangoapp/settings.py
+++ basedjangoapp/settings.py
@@ -131,6 +131,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
 PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
