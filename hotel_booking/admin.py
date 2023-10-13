56fbb7658034a1e109169b5616c998cd57870168
sudarshan519 <sudarshanshrestha519@gmail.com>
2023-10-13 19:52:33 +0545

    update

--- hotel_booking/admin.py
+++ hotel_booking/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from .models import *
 admin.site.register(Places)
 admin.site.register(Hotel)
-admin.site.register(HotelImages)
+# admin.site.register(HotelImages)
 admin.site.register(Room)
 admin.site.register(Booking)
 admin.site.register(BookingPayment)
